047c55edc2da21299a86fc03277c7deb9314781b
FernandaCG <fi.camposg@gmail.com>
2017-12-09 16:33:31 -0600

    Ajustes cliente

--- src/java/servlets/ServletArchivo.java
+++ src/java/servlets/ServletArchivo.java
@@ -16,88 +16,35 @@ import java.net.*;
  */
 @WebServlet(name = "ServletArchivo", urlPatterns = {"/ServletArchivo"})
 public class ServletArchivo extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             try {
                 File file = new File("/home/fernanda/Documentos/Redes2/5.jpg");
-                //Direccion IP del servidor.
-                Socket soc = new Socket("192.168.1.81", 2004);
-                byte[] bytes = new byte[16 * 1024];
-                InputStream in = new FileInputStream(file);
-                OutputStream dout = soc.getOutputStream();
-
-                int count;
-                while ((count = in.read(bytes)) > 0) {
-                    dout.write(bytes, 0, count);
+                try (
+                        //Direccion IP del servidor.
+                        Socket soc = new Socket("192.168.1.81", 2004)) {
+                    byte[] bytes = new byte[16 * 1024];
+                    InputStream in = new FileInputStream(file);
+                    try (OutputStream dout = soc.getOutputStream()) {
+                        int count;
+                        while ((count = in.read(bytes)) > 0) {
+                            dout.write(bytes, 0, count);
+                        }
+                    }
                 }
-
-                dout.close();
-                soc.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (IOException e) {
             }
-
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletArchivo</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletArchivo at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           
         }
     }
-
-// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+         
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
